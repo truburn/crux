b94a74d82c1779340abdd1728edcbf84242cbdc5
Jenny Seburn <jennifer.seburn@gmail.com>
2023-06-17 19:47:17 -0400

    add docs folder

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../docs/*.mdx",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
