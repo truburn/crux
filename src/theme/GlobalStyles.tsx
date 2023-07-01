043ef4cc5963687f0023176f0ee9293c35c4b9ef
Jenny Seburn <jennifer.seburn@gmail.com>
2023-07-01 18:08:33 -0400

    remove `useTheme`

--- src/theme/GlobalStyles.tsx
+++ src/theme/GlobalStyles.tsx
@@ -1,12 +1,11 @@
-import { Global, useTheme, css } from "@emotion/react";
+import { Global, css } from "@emotion/react";
 import { injectGlobal } from "@emotion/css";
+import { theme } from "@/theme";
 
 injectGlobal`@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;700&family=IBM+Plex+Mono:wght@200;400;700&family=Quicksand:wght@300;400;500;600;700&display=swap');`;
 
 // eslint-disable-next-line react-refresh/only-export-components
 export function useGlobalStyles() {
-  const theme = useTheme();
-
   return css({
     "html, body": {
       width: "100vw",
